cf57bc10fe058b151d2ff2a8dc6e416f6ec784ce
BurkeAkcakil <98296072+BurkeAkcakil@users.noreply.github.com>
2023-08-08 13:07:45 -0400

    I'm having a lot of problems trying to run the code, I will get back to it after studying a bit more on JS

--- javascript.js
+++ javascript.js
@@ -1,2 +1,40 @@
 console.log("Hello World")
 
+
+function getComputerChoice() {
+    const randomNumber = (Math.floor(Math.random() * 3));
+    if (randomNumber === 0) {
+        return "Rock";
+    }
+    else if (randomNumber === 1) {
+        return "Paper";
+    }
+    else if (randomNumber === 2) {
+        return "Scissors"
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!"
+    }
+    else if (playerSelection === "rock" && computerSelection === 2) {
+        return "You win! Rock beats Scissors"
+    }
+
+    else if (playerSelection === "paper" && computerSelection === 0) {
+        return "You win! Paper beats Rock!"
+    }
+
+    else if (playerSelection === "scissors" && computerSelection === 1) {
+        return "You win! Scissors beat Paper!"
+    }
+
+    else {
+        return "You lose!"
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
