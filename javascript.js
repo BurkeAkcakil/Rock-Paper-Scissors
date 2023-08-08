ad0b9f6c72afe707e8d4ac8cb3cbe675abd45fca
BurkeAkcakil <98296072+BurkeAkcakil@users.noreply.github.com>
2023-08-08 15:00:34 -0400

    Still debugging

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 console.log("Hello World")
 
 
+let playerSelection = prompt("What is your move?");
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const randomNumber = (Math.floor(Math.random() * 3));
     if (randomNumber === 0) {
@@ -14,27 +18,40 @@ function getComputerChoice() {
     }
 }
 
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "It's a tie!"
+        return "It's a tie!";
     }
-    else if (playerSelection === "rock" && computerSelection === 2) {
-        return "You win! Rock beats Scissors"
-    }
-
-    else if (playerSelection === "paper" && computerSelection === 0) {
-        return "You win! Paper beats Rock!"
+    else if (playerSelection === "rock" && computerSelection === "Scissors") {
+        return "You win! Rock beats Scissors!";
     }
 
-    else if (playerSelection === "scissors" && computerSelection === 1) {
-        return "You win! Scissors beat Paper!"
+    else if (playerSelection === "paper" && computerSelection === "Rock") {
+        return "You win! Paper beats Rock!";
+    }
+
+    else if (playerSelection === "scissors" && computerSelection === "Paper") {
+        return "You win! Scissors beat Paper!";
     }
 
     else {
-        return "You lose!"
+        return "You lose!";
     }
 }
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+let computerSelection = getComputerChoice();
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+        console.log(playerSelection);
+        let result = playRound(playerSelection.toLowerCase(), computerSelection);
+        console.log(result);
+        const score = "Player Score: " + playerScore + " CPU Score: " + computerScore
+        console.log(score);
+    }
+}
+
+game();
