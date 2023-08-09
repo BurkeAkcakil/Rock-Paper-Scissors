86ac888581d24118e393b309259e4dd0f14cf746
BurkeAkcakil <98296072+BurkeAkcakil@users.noreply.github.com>
2023-08-09 16:24:30 -0400

    Completely changed the code, initial test worked.

--- javascript.js
+++ javascript.js
@@ -1,57 +1,40 @@
 console.log("Hello World")
 
-
-let playerSelection = prompt("What is your move?");
-let playerScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     const randomNumber = (Math.floor(Math.random() * 3));
-    if (randomNumber === 0) {
-        return "Rock";
-    }
-    else if (randomNumber === 1) {
-        return "Paper";
-    }
-    else if (randomNumber === 2) {
-        return "Scissors"
+    switch (randomNumber) {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+            break;
     }
 }
-
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "It's a tie!";
     }
-    else if (playerSelection === "rock" && computerSelection === "Scissors") {
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
         return "You win! Rock beats Scissors!";
     }
-
-    else if (playerSelection === "paper" && computerSelection === "Rock") {
-        return "You win! Paper beats Rock!";
-    }
-
-    else if (playerSelection === "scissors" && computerSelection === "Paper") {
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
         return "You win! Scissors beat Paper!";
     }
-
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats Rock!";
+    }
     else {
         return "You lose!";
     }
 }
 
+//let playerSelection = prompt("What is your move? Rock, Paper or Scissors?").toLowerCase();
+let playerSelection = "Rock".toLowerCase();
+let computerSelection = getComputerChoice().toLowerCase();
 
-let computerSelection = getComputerChoice();
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-        console.log(playerSelection);
-        let result = playRound(playerSelection.toLowerCase(), computerSelection);
-        console.log(result);
-        const score = "Player Score: " + playerScore + " CPU Score: " + computerScore
-        console.log(score);
-    }
-}
-
-game();
+console.log(playRound(playerSelection, computerSelection));
